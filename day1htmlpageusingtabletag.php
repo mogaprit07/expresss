4106b9f117eb0a0cddfb034760081c2e9fa3d3c8
MOGAPRIT <84949958+mogaprit07@users.noreply.github.com>
2021-05-31 22:14:17 +0530

    day 1
    basic php

---
+++ day1htmlpageusingtabletag.php
@@ -0,0 +1,33 @@
+<html>
+    <body>
+        <table border="1" width="100%">
+            <tr>
+                <td>
+            <center> <font size='5'>PRIT MOGA </center>
+                </td>
+            </tr>
+            <tr>
+                <td>
+            <marquee bgcolor='pink'> hyy my name is prit </marquee>
+                </td>
+            </tr>
+            <tr>
+                <td bgcolor='yellow'>
+                  hyy i am studing in goverment engineering college modasa,
+                  4th year it student;
+                </td>
+            </tr>
+            <tr>
+                <td bgcolor='green'>
+                    created by me
+                </td>
+            </tr>>
+        </table>
+    </body>
+</html>
+
+
+<?php
+     echo "palanpur";
+     ?>
+
