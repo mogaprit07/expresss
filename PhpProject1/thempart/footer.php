8587a06e31e165b5e2bef3ee4c74614842ef9695
MOGAPRIT <84949958+mogaprit07@users.noreply.github.com>
2021-06-04 00:26:36 +0530

    day4
    Them  integration

---
+++ PhpProject1/thempart/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+	<p>2021.Website Manage By Prit Moga</p>
+</div>
---
+++ PhpProject1/thempart/sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar">
+    <h2>News</h2>
+    
+    <marquee>
+        Welcome to my website
+    </marquee>
+</div>
